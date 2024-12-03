c2998afc4392bbced49539252e7e0185de425127
sebastian <sebastian@imanku.co>
2024-12-03 13:57:13 -0500

    update

--- src/app/components/MainLateral.tsx
+++ src/app/components/MainLateral.tsx
@@ -1,8 +1,11 @@
 'use client'
 
-import React, { useState } from 'react'
-import { Box, Typography, TextField, InputAdornment, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
+import React, { useMemo, useState } from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { Box, Typography, TextField, FormControl, InputLabel, Select, MenuItem, useTheme, Autocomplete } from '@mui/material'
 import { SearchOutlined } from '@mui/icons-material'
+import { useMovie } from '@/context';
 
 
 const genres = [
@@ -12,37 +15,112 @@ const genres = [
   
 export const MainLateral = () => {
 
+    const theme = useTheme();
     const [genre] = useState('');
+    const [inputValue, setInputValue] = useState('');
+    const { movie } = useMovie();
+
+    const filteredOptions = useMemo(() => {
+        if (!movie?.results || !Array.isArray(movie.results)) {
+          return [];
+        }
+      
+        if (inputValue === '') {
+          return movie.results.slice(0, 3);
+        }
+      
+        return movie.results.filter(option =>
+          option.title.toLowerCase().includes(inputValue.toLowerCase())
+        );
+      }, [inputValue, movie]);
+      
       
     return (
     
     <Box sx={{ width: '100%', bgcolor: '#333', p: 2, color: '#fff' }}>
 
-        <Typography variant="h6" color="white" mb={2} mt={3}>
+        <Typography variant="h4" color="white" mb={2} mt={3}>
         Search
         </Typography>
-        <TextField
-        fullWidth
-        placeholder="Keywords"
-        variant="outlined"
-        size="small"
-        InputProps={{
-            endAdornment: (
-            <InputAdornment position="end">
-                <SearchOutlined sx={{ color: '#fff' }} />
-            </InputAdornment>
-            ),
-        }}
-        sx={{
-            bgcolor: '#444',
-            input: { color: '#fff' },
-            '.MuiOutlinedInput-notchedOutline': { borderColor: '#555' },
-        }}
-        />
+        <Box sx={{ display: 'flex', alignItems: 'center', position: 'relative', width: '100%',}}>
+            <Autocomplete
+              freeSolo
+              options={filteredOptions!}
+              getOptionLabel={(option) => typeof option === 'string' ? option : option.title}
+              renderOption={(props, option) => {
+                const { key, ...restProps } = props;
+              
+                return (
+                  <li key={key} {...restProps}>
+                    <Link
+                      href={`/movie/${option.id}`}
+                      passHref
+                      style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        width: '100%',
+                        textDecoration: 'none',
+                        color: 'inherit',
+                      }}
+                    >
+                      <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+                        <Image 
+                          src={`https://image.tmdb.org/t/p/w500/${option.backdrop_path}`}
+                          alt={option.title || 'Sin título'}
+                          width={50}
+                          height={50}
+                          objectFit="cover"
+                          style={{ marginRight: 10 }}
+                        />
+                        <Typography variant="body2">{option.title || 'Sin título'}</Typography>
+                      </Box>
+                    </Link>
+                  </li>
+                );
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  placeholder="Buscar ..."
+                  variant="outlined"
+                  size="small"
+                  sx={{
+                    backgroundColor: 'white',
+                    width: 280,
+                    '& .MuiOutlinedInput-root': {
+   
+                      '&:hover fieldset': {
+                        borderColor: 'rgba(0, 0, 0, 0.23)',
+                      },
+                      '&.Mui-focused fieldset': {
+                        borderColor: theme.palette.secondary.main,
+                        borderWidth: '2px',
+                      },
+                    },
+                    '& .MuiInputBase-input': {
+                      padding: '8px 12px',
+                    },
+                  }}
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <React.Fragment>
+                        {params.InputProps.endAdornment}
+                        <SearchOutlined/>
+                      </React.Fragment>
+                    ),
+                  }}  />
+              )}
+              onInputChange={(event, newInputValue) => {
+                setInputValue(newInputValue);
+              }}
+            />
+        </Box>
 
         <Typography variant="h6" color="white" mt={3} mb={2}>
         Genres
         </Typography>
+
         <FormControl fullWidth size="small" variant="outlined" sx={{ bgcolor: '#444' }}>
         <InputLabel sx={{ color: '#fff' }}>Selecciona Genero</InputLabel>
         <Select
--- src/app/movie/[id]/components/MovieContent.tsx
+++ src/app/movie/[id]/components/MovieContent.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { FC, useEffect } from 'react'
 import { Box } from '@mui/material';
 import { LayoutPrincipal } from '@/layouts/LayoutPrincipal';
@@ -15,10 +15,11 @@ export const MovieContent:FC<Props> = ({ idMovie }) => {
 
     const { OneIdMovie,RecommendationsMovies, setIdMovie,getRecommendations } = useMovie();
   
-    useEffect(() => {
-      setIdMovie('53c529a91fe5cf5cc90471988fa1254f',idMovie)
-      getRecommendations(idMovie,'53c529a91fe5cf5cc90471988fa1254f')
-    }, [setIdMovie,getRecommendations])
+// eslint-disable-next-line react-hooks/exhaustive-deps
+useEffect(() => {
+  setIdMovie('53c529a91fe5cf5cc90471988fa1254f', idMovie);
+  getRecommendations(idMovie, '53c529a91fe5cf5cc90471988fa1254f');
+}, []);
 
 
   return (
